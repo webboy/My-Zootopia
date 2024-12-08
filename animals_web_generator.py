f7bc0993629995e7710215e96e1ea6900595c7fe
webboy <extramedia.nemanja@gmail.com>
2024-12-08 14:58:42 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -13,17 +13,19 @@ def generate_animal_info(animals_data):
     for animal in animals_data:
         output += '<li class="cards__item">'
         if 'name' in animal:
-            output += f"Name: {animal['name']}<br/>\n"
+            output += f'  <div class="card__title">{animal["name"]}</div>\n'
 
+        output += '  <p class="card__text">\n'
         if 'characteristics' in animal and 'diet' in animal['characteristics']:
-            output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
+            output += f'      <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
 
         if 'locations' in animal and len(animal['locations']) > 0:
-            output += f"Location: {animal['locations'][0]}<br/>\n"
+            output += f'      <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
 
         if 'characteristics' in animal and 'type' in animal['characteristics']:
-            output += f"Type: {animal['characteristics']['type']}<br/>\n"
+            output += f'      <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
 
+        output += '  </p>\n'
         output += '</li>'
 
     return output
